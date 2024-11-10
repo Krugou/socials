94cb0a5180a4526145258b47b77889f75a195e57
Krugou <59641229+Krugou@users.noreply.github.com>
2024-11-10 17:07:05 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       strict: false
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/social' : ''
+      base: process.env.NODE_ENV === 'production' ? '/socials' : ''
     }
   }
 };
