33dd02fef13cbd4f4fc09a64a4887cc611463171
Aleksi Nokelainen <59641229+Krugou@users.noreply.github.com>
2025-06-07 20:57:19 +0000

    Fix class syntax for gradient background in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,15 @@
 </script>
 
 <div
-  class="flex min-h-screen flex-col"
+  class="flex min-h-screen flex-col bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]"
   class:bg-background={$isDarkMode}
   class:bg-white={!$isDarkMode}
   class:from-background={$isDarkMode}
-  class:from-purple-500/90={!$isDarkMode}
+  class:from-purple-500\/90={!$isDarkMode}
   class:via-background={$isDarkMode}
-  class:via-blue-900/90={!$isDarkMode}
+  class:via-blue-900\/90={!$isDarkMode}
   class:to-black={$isDarkMode}
-  class:to-blue-900/90={!$isDarkMode}
-  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]"
+  class:to-blue-900\/90={!$isDarkMode}
 >
   <Nav />
   <main class="flex-1">
